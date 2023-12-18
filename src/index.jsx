8ad65a550183a3e0e37917457e48bf111f4d950c
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2023-12-18 16:02:23 +0900

    Updates

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import '../assets/stylesheets/application.scss';
-import { createRoot } from "react-dom/client";
-import React from 'react';
-import App from './components/App';
-
-const container = document.querySelector("#root");
-const root = createRoot(container);
-root.render(<App />);
